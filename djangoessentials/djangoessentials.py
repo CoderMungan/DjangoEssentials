cdf94e8a3bec56b31ee85e3cd9d14008c725e011
CoderMungan <codermungan@gmail.com>
2025-01-27 13:47:29 +0300

    feat(s3): update the ACLs None

--- djangoessentials/djangoessentials.py
+++ djangoessentials/djangoessentials.py
@@ -4,4 +4,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MyS3Storage(S3Boto3Storage):
     location = "media/"  # S3'te files alt dir
     file_overwrite = False  # if have a same name about the file cant overwrite
-    default_acl = "public-read"  # just can read permission
+    default_acl = None  # just can read permission
